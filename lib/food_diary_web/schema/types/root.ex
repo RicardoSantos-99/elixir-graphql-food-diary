c7e09bb39436516ccece3cab759e7039fe00dc36
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-29 22:53:58 -0300

    feat: add trigger in create meal

--- lib/food_diary_web/schema/types/root.ex
+++ lib/food_diary_web/schema/types/root.ex
@@ -43,10 +43,17 @@ defmodule FoodDiaryWeb.Schema.Types.Root do
   end
 
   object :root_subscription do
+    # field :new_meal, :meal do
+    #   config fn _args, _info ->
+    #     {:ok, topic: "new_meal_topic"}
+    #   end
+
     field :new_meal, :meal do
       config fn _args, _info ->
         {:ok, topic: "new_meal_topic"}
       end
+
+      trigger :create_meal, topic: fn _ -> ["new_meal_topic"] end
     end
   end
 end
